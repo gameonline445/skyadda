d451f726f45c0847de39fa7030e6b7811a5a1fba
gameonline445 <nmbaar78@gmail.com>
2026-05-24 20:23:02 +0530

    Update wallet.js

--- wallet.js
+++ wallet.js
@@ -1,64 +1,26 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+let money = localStorage.getItem("wallet");
 
-import {
-getFirestore,
-doc,
-getDoc
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-import {
-getAuth,
-onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
-const firebaseConfig = {
-
-apiKey: "AIzaSyBNnnLu7HpCImLl5cIWKmB_eLjsbaR6zxg",
-
-authDomain: "skyadda-1d849.firebaseapp.com",
-
-projectId: "skyadda-1d849",
-
-storageBucket: "skyadda-1d849.firebasestorage.app",
-
-messagingSenderId: "929894889505",
-
-appId: "1:929894889505:web:6e76e0eb701ffe89d60b60"
-
-};
-
-const app = initializeApp(firebaseConfig);
-
-const db = getFirestore(app);
-
-const auth = getAuth(app);
-
-onAuthStateChanged(auth, async(user)=>{
-
-if(user){
-
-const docRef = doc(db,"users",user.uid);
-
-const docSnap = await getDoc(docRef);
-
-if(docSnap.exists()){
-
-document.getElementById("wallet").innerHTML =
-docSnap.data().wallet;
-
-document.getElementById("winning").innerHTML =
-docSnap.data().winning;
-
+if(money == null){
+money = 0;
+localStorage.setItem("wallet",0);
 }
 
+document.getElementById("wallet").innerHTML = money;
+
+document.getElementById("addmoney").onclick = function(){
+
+let add = prompt("Enter Amount");
+
+if(add != null){
+
+money = parseInt(money) + parseInt(add);
+
+localStorage.setItem("wallet",money);
+
+document.getElementById("wallet").innerHTML = money;
+
+alert("Money Added Successfully");
+
 }
-
-});
-
-document.getElementById("addmoney")
-.onclick = function(){
-
-window.location.href =
-"addmoney.html";
 
 }
