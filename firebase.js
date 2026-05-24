6e8c046356199cd867a213072d42c51c4e3b35fe
gameonline445 <nmbaar78@gmail.com>
2026-05-24 14:43:11 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+const firebaseConfig = {
+
+apiKey: "AIzaSyBNnnLu7HpCImLl5cIWKmB_eLjsbaR6zxg",
+
+authDomain: "skyadda-1d849.firebaseapp.com",
+
+projectId: "skyadda-1d849",
+
+storageBucket: "skyadda-1d849.firebasestorage.app",
+
+messagingSenderId: "929894889505",
+
+appId: "1:929894889505:web:6e76e0eb701ffe89d60b60",
+
+measurementId: "G-GPT48HDF0K"
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+console.log("Firebase Connected");
